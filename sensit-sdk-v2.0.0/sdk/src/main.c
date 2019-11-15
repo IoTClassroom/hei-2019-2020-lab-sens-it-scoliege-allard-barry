d522e6ab91696a527bf700a8f180054f55764b0e
Paul Scoliege <paul.scoliege@hei.yncrea.fr>
2019-11-15 15:24:05 +0100

    1ere modif du main.c

--- sensit-sdk-v2.0.0/sdk/src/main.c
+++ sensit-sdk-v2.0.0/sdk/src/main.c
@@ -1,11 +1,10 @@
 /*!******************************************************************
- * \file main.c
- * \brief Sens'it SDK template
+ * \file main_VIBRATION.c
+ * \brief Sens'it Discovery mode Vibration demonstration code
  * \author Sens'it Team
  * \copyright Copyright (c) 2018 Sigfox, All Rights Reserved.
  *
- * This file is an empty main template.
- * You can use it as a basis to develop your own firmware.
+ * For more information on this firmware, see vibration.md.
  *******************************************************************/
 /******* INCLUDES **************************************************/
 #include "sensit_types.h"
@@ -14,127 +13,145 @@
 #include "button.h"
 #include "battery.h"
 #include "radio_api.h"
-#include "hts221.h"
-#include "ltr329.h"
 #include "fxos8700.h"
 #include "discovery.h"
 
+
+/******** DEFINES **************************************************/
+#define VIBRATION_THRESHOLD                0x10 /* With 2g range, 3,9 mg threshold */
+#define VIBRATION_COUNT                    2
+
 /******* GLOBAL VARIABLES ******************************************/
-u8 firmware_version[] = "TEMPLATE";
+u8 firmware_version[] = "VIBR_v2.0.0";
 
-/*******************************************************************/
-
-int main()
+/**
+ * Init sigfox module
+ */
+static void init()
 {
+    SENSIT_API_configure_button(INTERRUPT_BOTH_EGDE); /* Configure button */
+
     error_t err;
-    button_e btn;
-    u16 battery_level;
-    bool send = FALSE;
 
-    /* Start of initialization */
-
-    /* Configure button */
-    SENSIT_API_configure_button(INTERRUPT_BOTH_EGDE);
-
-    /* Initialize Sens'it radio */
-    err = RADIO_API_init();
+    err = RADIO_API_init(); /* Initialize Sens'it radio */
     ERROR_parser(err);
 
-    /* Initialize temperature & humidity sensor */
-    err = HTS221_init();
+    err = FXOS8700_init(); /* Initialize accelerometer */
     ERROR_parser(err);
 
-    /* Initialize light sensor */
-    err = LTR329_init();
-    ERROR_parser(err);
-
-    /* Initialize accelerometer */
-    err = FXOS8700_init();
-    ERROR_parser(err);
+    /* Put accelerometer in transient mode */
+    FXOS8700_set_transient_mode(FXOS8700_RANGE_2G, VIBRATION_THRESHOLD, VIBRATION_COUNT);
 
     /* Clear pending interrupt */
     pending_interrupt = 0;
+}
 
-    /* End of initialization */
-
-    while (TRUE)
+typedef struct
+{
+    struct {
+        s8 eventId:4;
+        s8 reserved:4;
+    };
+    struct
     {
-        /* Execution loop */
+        s16 ax;
+        s16 ay;
+        s16 az;
+    };
+} __attribute__((packed)) data_t;
 
-        /* Check of battery level */
-        BATTERY_handler(&battery_level);
+/**
+ * Send data by sigfox
+ * @param data
+ * @return
+ */
+static void sendData(data_t *data)
+{
+    /* Send the message */
+    error_t err = RADIO_API_send_message(RGB_BLUE, (u8 *) data, sizeof(data_t), FALSE, NULL);
+    ERROR_parser(err);/* Parse the error code */
+}
+
+#define CHECK_EVENT(evt) (pending_interrupt & evt) == evt
+#define CLEAR_EVENT(evt) pending_interrupt &= ~evt
+
+/**
+ * Convert value from little endian to Big endian
+ * @param num s16 value in little endian
+ * @return s16 value big endian
+ */
+s16 convertLittleToBig(s16 num) {
+    return (num>>8) | (num<<8);
+}
+
+/**
+ * Get acceleration information from sensor
+ * @param data
+ */
+static void getAccInfo(data_t *data)
+{
+    fxos8700_data_s info;
+    error_t err = FXOS8700_read_acceleration(&info);
+
+    ERROR_parser(err);
+
+    data->eventId = 1;
+
+    data->ax = convertLittleToBig(info.x);
+    data->ay = convertLittleToBig(info.y);
+    data->az = convertLittleToBig(info.z);
+}
+
+int main()
+{
+    init();
+    bool send = FALSE;
+
+    while (TRUE) {
+        data_t data = {0};
 
         /* RTC alarm interrupt handler */
-        if ((pending_interrupt & INTERRUPT_MASK_RTC) == INTERRUPT_MASK_RTC)
-        {
-            /* Clear interrupt */
-            pending_interrupt &= ~INTERRUPT_MASK_RTC;
+        if (CHECK_EVENT(INTERRUPT_MASK_RTC)) {
+            CLEAR_EVENT(INTERRUPT_MASK_RTC);
         }
 
         /* Button interrupt handler */
-        if ((pending_interrupt & INTERRUPT_MASK_BUTTON) == INTERRUPT_MASK_BUTTON)
-        {
-            /* RGB Led ON during count of button presses */
-            SENSIT_API_set_rgb_led(RGB_WHITE);
+        if (CHECK_EVENT(INTERRUPT_MASK_BUTTON)) {
+            SENSIT_API_set_rgb_led(RGB_BLUE); /* RGB Led ON during count of button presses */
+            button_e btn = BUTTON_handler(); /* Count number of presses */
+            SENSIT_API_set_rgb_led(RGB_OFF); /* RGB Led OFF */
 
-            /* Count number of presses */
-            btn = BUTTON_handler();
-
-            /* RGB Led OFF */
-            SENSIT_API_set_rgb_led(RGB_OFF);
-
-            if (btn == BUTTON_THREE_PRESSES)
-            {
-                /* Force a RTC alarm interrupt to do a new measurement */
-                pending_interrupt |= INTERRUPT_MASK_RTC;
-
-                /* Set send Sigfox */
+            if (btn == BUTTON_TWO_PRESSES) {
+                getAccInfo(&data);
                 send = TRUE;
-            }
-            else if (btn == BUTTON_FOUR_PRESSES)
-            {
-                /* Reset the device */
-                SENSIT_API_reset();
+            } else if (btn == BUTTON_FOUR_PRESSES) {
+                SENSIT_API_reset(); /* Reset the device */
             }
 
-            /* Clear interrupt */
-            pending_interrupt &= ~INTERRUPT_MASK_BUTTON;
+            CLEAR_EVENT(INTERRUPT_MASK_BUTTON);
         }
 
         /* Reed switch interrupt handler */
-        if ((pending_interrupt & INTERRUPT_MASK_REED_SWITCH) == INTERRUPT_MASK_REED_SWITCH)
-        {
-            /* Clear interrupt */
-            pending_interrupt &= ~INTERRUPT_MASK_REED_SWITCH;
+        if (CHECK_EVENT(INTERRUPT_MASK_REED_SWITCH)) {
+            CLEAR_EVENT(INTERRUPT_MASK_REED_SWITCH);
         }
 
         /* Accelerometer interrupt handler */
-        if ((pending_interrupt & INTERRUPT_MASK_FXOS8700) == INTERRUPT_MASK_FXOS8700)
-        {
-            /* Clear interrupt */
-            pending_interrupt &= ~INTERRUPT_MASK_FXOS8700;
+        if (CHECK_EVENT(INTERRUPT_MASK_FXOS8700)) {
+            getAccInfo(&data);
+            send = TRUE;
+            CLEAR_EVENT(INTERRUPT_MASK_FXOS8700);
         }
 
         /* Check if we need to send a message */
-        if (send == TRUE)
-        {
-
-            /* Send the message */
-            err = RADIO_API_send_message(RGB_MAGENTA, (u8 *)"HI", 2, FALSE, NULL);
-            /* Parse the error code */
-            ERROR_parser(err);
-
-            /* Clear send flag */
+        if (send == TRUE) {
+            sendData(&data);
             send = FALSE;
         }
 
         /* Check if all interrupt have been clear */
-        if (pending_interrupt == 0)
-        {
-            /* Wait for Interrupt */
-            SENSIT_API_sleep(FALSE);
+        if (pending_interrupt == 0) {
+            SENSIT_API_sleep(FALSE); /* Wait for Interrupt */
         }
     } /* End of while */
 }
-
-/*******************************************************************/
